d68a352e60ff3bc853b0172a419dbf69fb6f55c7
Morgan Abel <morganabel@gmail.com>
2025-11-22 00:07:12 -0500

    android jsi

--- apps/mobile/android/app/src/main/java/com/anonymous/workoutagentcemobile/MainApplication.kt
+++ apps/mobile/android/app/src/main/java/com/anonymous/workoutagentcemobile/MainApplication.kt
@@ -12,6 +12,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.react.soloader.OpenSourceMergedSoMapping
 import com.facebook.soloader.SoLoader
+import com.nozbe.watermelondb.jsi.WatermelonDBJSIPackage
 
 import expo.modules.ApplicationLifecycleDispatcher
 import expo.modules.ReactNativeHostWrapper
@@ -25,6 +26,7 @@ class MainApplication : Application(), ReactApplication {
             val packages = PackageList(this).packages
             // Packages that cannot be autolinked yet can be added manually here, for example:
             // packages.add(MyReactNativePackage())
+            packages.add(WatermelonDBJSIPackage())
             return packages
           }
 
